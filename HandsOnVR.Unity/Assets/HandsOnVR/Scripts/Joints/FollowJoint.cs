ae0252064f1bbb89e85cc362b1643ff44e51003f
David Barlia <barliesque@gmail.com>
2019-11-05 08:04:22 -0800

    FollowTransform renamed to FollowJoint, implementing new IJoint interface

--- HandsOnVR.Unity/Assets/HandsOnVR/Scripts/Joints/FollowJoint.cs
+++ HandsOnVR.Unity/Assets/HandsOnVR/Scripts/Joints/FollowJoint.cs
@@ -1,22 +1,30 @@
 ﻿using UnityEngine;
+using UnityEngine.Serialization;
 
 
 namespace HandsOnVR
 {
 	/// <summary>
-	/// Make a Rigidbody follow a transform, in a non-kinematic manner.
+	/// Make this object follow a transform, in a physics-based manner.  May be paired with the PropAttacher component.
 	/// </summary>
 	[RequireComponent(typeof(Rigidbody))]
 	[ExecuteInEditMode]
-	public class FollowTransform : MonoBehaviour
+	public class FollowJoint : MonoBehaviour, IJoint
 	{
-		public Transform Target;
+		[FormerlySerializedAs("Target")] public Transform _connectedTo;
+		public Transform ConnectedTo
+		{
+			get => _connectedTo;
+			set => _connectedTo = value;
+		}
+		
 		public float MoveSpeed = 60f;
 		[Range(0f, 1f)] public float TurnSpeed = 0.9f;
 
 		[Tooltip("When the target is further than this distance, the Rigidbody's position will be matched immediately.")]
 		public float TeleportThreshold = 0.25f;
 
+		Transform _xform;
 		Rigidbody _body;
 
 
@@ -35,10 +43,10 @@ namespace HandsOnVR
 			if (!Application.isPlaying)
 			{
 				// Make sure we have a Rigidbody
-				if (_body == null)
+				if (!_body)
 				{
 					_body = GetComponent<Rigidbody>();
-					if (_body == null)
+					if (!_body)
 					{
 						_body = gameObject.AddComponent<Rigidbody>();
 					}
@@ -48,13 +56,13 @@ namespace HandsOnVR
 				_body.isKinematic = false;
 				_body.useGravity = false;
 
-				if (Target == null)
+				if (!_connectedTo)
 				{
 					return;
 				}
 				// Match the target position/rotation immediately
-				transform.position = Target.position;
-				transform.rotation = Target.rotation;
+				_xform.position = _connectedTo.position;
+				_xform.rotation = _connectedTo.rotation;
 			}
 		}
 #endif
@@ -62,11 +70,15 @@ namespace HandsOnVR
 
 		private void OnEnable()
 		{
-			if (Target != null)
+			if (_xform == null)
+			{
+				_xform = GetComponent<Transform>();
+			}
+			if (_connectedTo != null)
 			{
 				// Match the target position/rotation immediately
-				transform.position = Target.position;
-				transform.rotation = Target.rotation;
+				_xform.position = _connectedTo.position;
+				_xform.rotation = _connectedTo.rotation;
 				if (_body != null)
 				{
 					_body.velocity = Vector3.zero;
@@ -78,14 +90,14 @@ namespace HandsOnVR
 
 		void FixedUpdate()
 		{
-			if (Target != null)
+			if (_connectedTo)
 			{
-				var delta = (Target.position - _body.position);
+				var delta = (_connectedTo.position - _body.position);
 
 				if (delta.sqrMagnitude >= (TeleportThreshold * TeleportThreshold))
 				{
 					// Teleport Rigidbody to target position
-					_body.position = Target.position;
+					_body.position = _connectedTo.position;
 					_body.velocity = Vector3.zero;
 				} else
 				{
@@ -94,7 +106,7 @@ namespace HandsOnVR
 				}
 
 				// Find the angular delta, and convert from Quaternion to Angle/Axis
-				var angDelta = Target.rotation * Quaternion.Inverse(_body.rotation);
+				var angDelta = _connectedTo.rotation * Quaternion.Inverse(_body.rotation);
 				float angle;
 				Vector3 axis;
 				angDelta.ToAngleAxis(out angle, out axis);
---
+++ HandsOnVR.Unity/Assets/HandsOnVR/Scripts/Joints/IJoint.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace HandsOnVR
+{
+
+	/// <summary>
+	/// An interface allowing joints to be paired with the PropAttacher component.
+	/// </summary>
+	public interface IJoint
+	{
+		Transform ConnectedTo { get; set; }
+
+	}
+}
