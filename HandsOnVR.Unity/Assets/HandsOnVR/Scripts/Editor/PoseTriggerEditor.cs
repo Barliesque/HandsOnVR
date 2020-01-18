d55418b2362b40e579e998b133660563f1e75bdb
David Barlia <barliesque@gmail.com>
2020-01-18 11:17:00 -0800

    Added PoseTrigger to enable proximity poses without a Grabbable

---
+++ HandsOnVR.Unity/Assets/HandsOnVR/Scripts/Editor/PoseTriggerEditor.cs
@@ -0,0 +1,41 @@
+﻿using Barliesque.InspectorTools.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace HandsOnVR.Editor
+{
+
+    [CustomEditor(typeof(PoseTrigger))]
+    public class PoseTriggerEditor : EditorBase<PoseTrigger>
+    {
+        protected override void CustomInspector(PoseTrigger inst)
+        {
+            PropertyField("_proximityPose");
+
+            EditorTools.Header("Hands");
+            PropertyField("_leftHand");
+            PropertyField("_rightHand");
+
+            var collider = inst.GetComponents<Collider>();
+            var body = inst.GetComponentInParent<Rigidbody>();
+
+            if (collider.Length == 0)
+            {
+                EditorTools.HelpBox("A collider is required on this GameObject to enable the PoseTrigger to function.", MessageType.Error);
+            }
+            else if (collider.Length > 1)
+            {
+                EditorTools.HelpBox("Only one collider may be used per PoseTrigger.", MessageType.Warning);
+            }
+            else if (!collider[0].isTrigger)
+            {
+                EditorTools.HelpBox("The collider must be set to be a trigger.", MessageType.Warning);
+            }
+            else if (!body)
+            {
+                EditorTools.HelpBox("A Rigidbody component is required on this or a parent GameObject.", MessageType.Error);
+            }
+
+        }
+    }
+}
---
+++ HandsOnVR.Unity/Assets/HandsOnVR/Scripts/PoseTrigger.cs
@@ -0,0 +1,51 @@
+﻿using Barliesque.InspectorTools;
+using UnityEngine;
+
+
+namespace HandsOnVR
+{
+
+	/// <summary>
+	/// When the player's hand comes within range of an object (defined by a trigger collider) this component changes the hand's pose.
+	/// </summary>
+	public class PoseTrigger : MonoBehaviour
+    {
+
+		[Tooltip("A bool parameter name found in the Animator components of the player's hands.  While the player's hand is near this object, the specified parameter will be set to true.")]
+		[SerializeField] string _proximityPose;
+		public string ProximityPose => _proximityPose;
+
+		int _proximityPoseID;
+		public int ProximityPoseID
+		{
+			get => _proximityPoseID;
+			private set => _proximityPoseID = value;
+		}
+
+		[Tooltip("Should the right hand respond to this PoseTrigger?")]
+		[SerializeField] bool _rightHand = true;
+
+		[Tooltip("Should the left hand respond to this PoseTrigger?")]
+		[SerializeField] bool _leftHand = true;
+
+		public Collider Collider { get; private set; }
+
+
+		private void Awake()
+		{
+			Collider = GetComponent<Collider>();
+			ProximityPoseID = Animator.StringToHash(_proximityPose);
+		}
+
+		public bool SupportsHand(Hand hand)
+		{
+			return (hand == Hand.Left) ? _leftHand : _rightHand;
+		}
+
+
+		//TODO  The Grabber needs to gather trigger colliders as it does for Grabbables, responding with the same logic
+		//TODO  The editor of this component should show a warning info box that it requires a trigger collider to function
+
+	}
+
+}
