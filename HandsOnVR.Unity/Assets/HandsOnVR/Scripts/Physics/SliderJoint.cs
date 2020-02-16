06e7c51a439ee6b0d6e6c032576bb64d0709de84
David Barlia <barliesque@gmail.com>
2020-02-16 12:58:41 -0800

    SliderJoint:  Made Body property public

--- HandsOnVR.Unity/Assets/HandsOnVR/Scripts/Physics/SliderJoint.cs
+++ HandsOnVR.Unity/Assets/HandsOnVR/Scripts/Physics/SliderJoint.cs
@@ -1,4 +1,5 @@
 ﻿using Barliesque.InspectorTools;
+using System;
 using UnityEngine;
 
 
@@ -8,9 +9,11 @@ namespace HandsOnVR
 	/// At runtime, this component generates a Prismatic Joint -- a ConfigurableJoint with its numerous properties automatically set up to act as a linear sliding joint.
 	/// </summary>
 	/// https://gamedev.stackexchange.com/questions/129659/how-do-i-configure-a-joint-for-a-sliding-door-in-unity
-	[RequireComponent(typeof(Rigidbody))]
 	public class SliderJoint : MonoBehaviour, IJoint
 	{
+		[Tooltip("The Rigidbody of this object, usually located on the same GameObject as the SliderJoint")]
+		public Rigidbody Body;
+
 		[Tooltip("A reference to another rigidbody this joint connects to.")]
 		public Rigidbody ConnectedBody;
 
@@ -34,19 +37,24 @@ namespace HandsOnVR
 
 		ConfigurableJoint _joint;
 		Transform _connectedXform;
-		Rigidbody _body;
 
 
-		private void Awake()
+		private void Start()
 		{
-			_joint = gameObject.AddComponent<ConfigurableJoint>();
+			if (!Body) Body = GetComponentInParent<Rigidbody>();
+			if (!Body)
+			{
+				throw new Exception("SliderJoint requires a Rigidbody");
+			}
+
+			_joint = Body.gameObject.AddComponent<ConfigurableJoint>();
 			UpdateJoint();
 		}
 
 		public void UpdateJoint()
 		{
 			_joint.connectedBody = ConnectedBody;
-			_connectedXform = ConnectedBody?.transform ?? null;
+			_connectedXform = ConnectedBody ? ConnectedBody.transform : null;
 
 			_joint.autoConfigureConnectedAnchor = false;
 
@@ -68,8 +76,8 @@ namespace HandsOnVR
 			_joint.axis = Vector3.right;
 			_joint.anchor = AnchorPosition - (axis * Limit * 0.5f);
 
-			_body = GetComponent<Rigidbody>();
-			_body.constraints = RigidbodyConstraints.None;
+			if (!Body) Body = GetComponentInParent<Rigidbody>();
+			Body.constraints = RigidbodyConstraints.None;
 		}
 
 
