1e28a0102931663940da25a29a4e3d7243cdcb7e
David Barlia <barliesque@gmail.com>
2020-02-21 10:50:30 -0800

    Added utility class Limits and a property drawer for the Inspector.

---
+++ HandsOnVR.Unity/Assets/HandsOnVR/Scripts/Utils/Editor/LimitsDrawer.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+
+namespace HandsOnVR.Editor
+{
+
+    [CustomPropertyDrawer(typeof(Limits))]
+    public class LimitsDrawer : PropertyDrawer
+    {
+
+        public override VisualElement CreatePropertyGUI(SerializedProperty property)
+        {
+            // Create property container element.
+            var container = new VisualElement();
+
+            // Create property fields.
+            var lowField = new PropertyField(property.FindPropertyRelative("Low"));
+            var highField = new PropertyField(property.FindPropertyRelative("High"));
+
+            // Add fields to the container.
+            container.Add(lowField);
+            container.Add(highField);
+
+            return container;
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            // Using BeginProperty / EndProperty on the parent property means that
+            // prefab override logic works on the entire property.
+            EditorGUI.BeginProperty(position, label, property);
+
+            // Draw label
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+            // Don't make child fields be indented
+            var indent = EditorGUI.indentLevel;
+            EditorGUI.indentLevel = 0;
+
+            // Calculate rects
+            var width = position.width * 0.5f - 26f;
+            var lowRect = new Rect(position.x, position.y, width, position.height);
+            var toRect = new Rect(position.x + width + 6f, position.y, 14f, position.height);
+            var highRect = new Rect(position.x + width + 26, position.y, width, position.height);
+
+            // Draw fields - passs GUIContent.none to each so they are drawn without labels
+            EditorGUI.PropertyField(lowRect, property.FindPropertyRelative("Low"), GUIContent.none);
+            EditorGUI.LabelField(toRect, "to");
+            EditorGUI.PropertyField(highRect, property.FindPropertyRelative("High"), GUIContent.none);
+
+            // Set indent back to what it was
+            EditorGUI.indentLevel = indent;
+
+            EditorGUI.EndProperty();
+        }
+
+    }
+}
---
+++ HandsOnVR.Unity/Assets/HandsOnVR/Scripts/Utils/Limits.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace HandsOnVR
+{
+
+	[Serializable]
+	public struct Limits
+	{
+		public float Low;
+		public float High;
+
+		public Limits(float low, float high)
+		{
+			Low = low;
+			High = high;
+		}
+
+		public bool IsInside(float value) => (Low < High) ? (value >= Low && value <= High) : (value <= Low && value >= High);
+		public float Clamp(float value) => (Low < High) ? Mathf.Clamp(value, Low, High) : Mathf.Clamp(value, High, Low);
+		public float Range => High - Low;
+		public float Normalize(float value) => Mathf.InverseLerp(Low, High, value);
+		public float Lerp(float value) => Mathf.Lerp(Low, High, value);
+	}
+}
