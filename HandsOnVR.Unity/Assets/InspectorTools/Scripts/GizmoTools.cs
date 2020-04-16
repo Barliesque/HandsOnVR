aafbd55113a72c0d10e4041620e34ba918e326f3
David Barlia <barliesque@gmail.com>
2020-04-15 19:38:45 -0700

    Added GizmoTools for custom scene view gizmos

---
+++ HandsOnVR.Unity/Assets/InspectorTools/Scripts/GizmoTools.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Barliesque.InspectorTools
+{
+	public class GizmoTools
+	{
+		static public void DrawAxes(Vector3 pos, Quaternion rot, float alpha = 1f)
+		{
+			Gizmos.color = new Color(1, 0, 0, alpha);
+			Gizmos.DrawLine(pos, pos + (rot * Vector3.right) * 0.05f);
+			Gizmos.color = new Color(0, 0, 1, alpha);
+			Gizmos.DrawLine(pos, pos + (rot * Vector3.forward) * 0.05f);
+			Gizmos.color = new Color(0, 1, 0, alpha);
+			Gizmos.DrawLine(pos, pos + (rot * Vector3.up) * 0.05f);
+		}
+	}
+}
