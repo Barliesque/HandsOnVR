06b257f611396c405083d914ad47907db8b550ee
David Barlia <barliesque@gmail.com>
2020-05-15 11:54:31 -0700

    Added extension method Vector3.GetAxis()

---
+++ HandsOnVR.Unity/Assets/HandsOnVR/Scripts/Extensions/Vector3Extensions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace HandsOnVR
+{
+	static public class Vector3Extensions
+	{
+		static public float GetAxis(this Vector3 vec3, Axis axis)
+		{
+			return axis == Axis.X ? vec3.x : (axis == Axis.Y ? vec3.y : vec3.z);
+		}
+	}
+}
