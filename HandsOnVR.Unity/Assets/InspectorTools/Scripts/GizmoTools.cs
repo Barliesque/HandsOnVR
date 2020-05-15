368dead4bb02b080f8397fdcb66755298978e3dc
David Barlia <barliesque@gmail.com>
2020-05-15 11:51:30 -0700

    Added GizmoTools.DrawRotatedCube()

--- HandsOnVR.Unity/Assets/InspectorTools/Scripts/GizmoTools.cs
+++ HandsOnVR.Unity/Assets/InspectorTools/Scripts/GizmoTools.cs
@@ -2,8 +2,45 @@
 
 namespace Barliesque.InspectorTools
 {
-	public class GizmoTools
+	static public class GizmoTools
 	{
+		static private readonly Mesh _quad = new Mesh()
+		{
+			vertices = new[]
+			{
+				new Vector3(-0.5f, 0.5f, 0f),
+				new Vector3(0.5f, 0.5f, 0f),
+				new Vector3(0.5f, -0.5f, 0f),
+				new Vector3(-0.5f, -0.5f, 0f),
+			},
+			normals = new[]
+			{
+				Vector3.back, Vector3.back, Vector3.back, Vector3.back
+			},
+			triangles = new[]
+			{
+				0, 1, 2, 0, 2, 3
+			},
+			uv = new[]
+			{
+				new Vector2(0, 1),
+				new Vector2(1, 1),
+				new Vector2(1, 0),
+				new Vector2(0, 0)
+			}
+		};
+
+		static private readonly Vector3[] _cubeSides =
+		{
+			Vector3.forward, Vector3.right, Vector3.up, Vector3.back, Vector3.left, Vector3.down
+		};
+
+		static private readonly Color[] _axisColors =
+		{
+			Color.blue * 0.75f, Color.red * 0.75f, Color.green * 0.5f, Color.blue, Color.red, Color.green
+		};
+
+
 		static public void DrawAxes(Vector3 pos, Quaternion rot, float alpha = 1f)
 		{
 			Gizmos.color = new Color(1, 0, 0, alpha);
@@ -12,6 +49,54 @@ namespace Barliesque.InspectorTools
 			Gizmos.DrawLine(pos, pos + (rot * Vector3.forward) * 0.05f);
 			Gizmos.color = new Color(0, 1, 0, alpha);
 			Gizmos.DrawLine(pos, pos + (rot * Vector3.up) * 0.05f);
+		}
+
+		static public void DrawAxes(Vector3 pos, Quaternion rot, float alphaX, float alphaY, float alphaZ)
+		{
+			Gizmos.color = new Color(1, 0, 0, alphaX);
+			Gizmos.DrawLine(pos, pos + (rot * Vector3.right) * 0.05f);
+			Gizmos.color = new Color(0, 0, 1, alphaY);
+			Gizmos.DrawLine(pos, pos + (rot * Vector3.forward) * 0.05f);
+			Gizmos.color = new Color(0, 1, 0, alphaZ);
+			Gizmos.DrawLine(pos, pos + (rot * Vector3.up) * 0.05f);
+		}
+
+
+		static public void DrawRotatedCube(Vector3 pos, Quaternion rot, Vector3 size)
+		{
+			var scale = new[]
+			{
+				new Vector3(size.x, size.y, 1f),
+				new Vector3(size.z, size.y, 1f),
+				new Vector3(size.x, size.z, 1f)
+			};
+			for (var i = 0; i < 6; i++)
+			{
+				var side = _cubeSides[i];
+				var orientation = rot * Quaternion.LookRotation(side);
+
+				var r = side;
+				r.Scale(size);
+				var radius = Mathf.Abs(r.x + r.y + r.z) * 0.5f;
+
+				Gizmos.DrawMesh(_quad, pos + (orientation * (Vector3.back * radius)), orientation, scale[i % 3]);
+			}
+		}
+
+
+		static public void DrawRotatedCube(Vector3 pos, Quaternion rot, float size, bool useAxisColors = false)
+		{
+			var radius = size * 0.5f;
+			var scale = Vector3.one * size;
+
+			for (var i = 0; i < _cubeSides.Length; i++)
+			{
+				var side = _cubeSides[i];
+				if (useAxisColors) Gizmos.color = _axisColors[i];
+				var orientation = rot * Quaternion.LookRotation(side);
+
+				Gizmos.DrawMesh(_quad, pos + (orientation * (Vector3.back * radius)), orientation, scale);
+			}
 		}
 	}
 }
